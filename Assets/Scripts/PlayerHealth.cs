427d0c9eb68206f17813615b0abc7b1d97c88c37
weixiangbeak <73181874+weixiangbeak@users.noreply.github.com>
2021-08-02 17:06:48 +0800

    added the changing to game over screen
    added the changing to game over screen

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -20,5 +21,6 @@ public class PlayerHealth : MonoBehaviour
     {
         //show game over UI
         Time.timeScale = 0;
+        SceneManager.LoadScene(2);
     }
 }
