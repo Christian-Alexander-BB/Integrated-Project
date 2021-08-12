73facc937d36d4fd84da3f17dec5b7eb53da91f1
darriktan <s10208603@connect.np.edu.sg>
2021-08-13 00:46:42 +0800

    teammate stuff

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -13,9 +13,6 @@ public class Bullet : MonoBehaviour
     // player
     public GameObject target;
 
-    // timer
-    public float timer;
-
     // Update is called once per frame
     void Update()
     {
@@ -23,23 +20,11 @@ public class Bullet : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
     }
 
-    void FixedUpdate()
-    {
-        timer += 1;
-
-        if (timer >= 100)
-        {
-            target.GetComponent<PlayerHealth>().notTakingDamage = true;
-        }
-
-    }
-
     void OnTriggerEnter(Collider other)
     {
         // if the bullet hits the player, decreases the health of the player and destroys the bullet
         if (other.tag == "Player")
         {
-            timer = 0;
             target = other.gameObject;
             // player takes damage
             target.GetComponent<PlayerHealth>().health -= damage;
